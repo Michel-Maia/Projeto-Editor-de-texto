c9e954e578f8e89d07947c12e9c89cd63256d4e6
Michel Maia <programador.michel.maia@gmail.com>
2022-03-08 08:34:29 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TextEditor
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
