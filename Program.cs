b8acbb80659bf4b0096c213cbf876a2a92fd41d1
Michel Maia <programador.michel.maia@gmail.com>
2022-03-08 09:24:48 -0300

    commit menu

--- Program.cs
+++ Program.cs
@@ -8,7 +8,43 @@ namespace TextEditor
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
+        }
+
+        static void Menu() {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer ?");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("0 - Sair");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Abrir(){
+
+        }
+
+        static void Editar(){
+            Console.Clear(); 
+            Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
+            Console.WriteLine("----------------");
+            string text = "";
+
+            do {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Console.WriteLine(text);
         }
     }
 }
