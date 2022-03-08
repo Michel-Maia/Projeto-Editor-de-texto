4c4f868197ca3a12e1b482dc10f738fde02181d2
Michel Maia <programador.michel.maia@gmail.com>
2022-03-08 10:29:53 -0300

    commit abrir editar

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace TextEditor
 {
@@ -29,6 +30,19 @@ namespace TextEditor
         }
 
         static void Abrir(){
+            Console.Clear();
+            Console.WriteLine("Qual caminho do arquivo ?");
+            string path = Console.ReadLine();
+
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
 
         }
 
@@ -45,6 +59,24 @@ namespace TextEditor
             while (Console.ReadKey().Key != ConsoleKey.Escape);
 
             Console.WriteLine(text);
+            Salvar(text);
+        }
+
+        static void Salvar(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("Qual caminho para salvar o arquivo ?");
+            var path = Console.ReadLine();
+
+            using(var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            } 
+
+            Console.WriteLine($"Arquivo {path} salvo com sucesso!");
+            Console.ReadLine();
+            Menu();
+
         }
     }
 }
